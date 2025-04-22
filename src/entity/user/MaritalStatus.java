bd9fb650968a4c0baf9b1beda761f45f72ae9403
sathwiik <123555744+sathwiik@users.noreply.github.com>
2025-04-22 14:35:34 +0800

    Create MaritalStatus.java

---
+++ src/entity/user/MaritalStatus.java
@@ -0,0 +1,17 @@
+package entity.user;
+
+/**
+ * Represents the marital status of a user within the system.
+ * Used primarily for determining eligibility for certain housing options or benefits.
+ */
+public enum MaritalStatus {
+    /**
+     * Represents the 'Single' marital status.
+     */
+    SINGLE,
+
+    /**
+     * Represents the 'Married' marital status.
+     */
+    MARRIED
+}
